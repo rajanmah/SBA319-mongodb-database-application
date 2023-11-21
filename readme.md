c2e0044f4dcdcc94754e649479451c5c20f6e979
rajanmah <146156379+rajanmah@users.noreply.github.com>
2023-11-21 00:46:52 -0500

    GET ONE and POST create one tested

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # SBA 219 MongoDB Server Application
-##I have used 'sample_supplies' from mongoDB sample database to apply CRUD operations to its 'sales' data using mongoDB and express.
+##I have used 'sample_supplies' from mongoDB sample database to apply CRUD operations to its 'sales' data using mongoDB and express. I used 'reqbin.com' platform to test CRUD Operatons. I also used 'Rest Client' VS Code extension for that purpose.
 
--- routes/supplies.mjs
+++ routes/supplies.mjs
@@ -13,12 +13,35 @@ router.get('/', async (req, res) => {
     res.send(results).status(200)
 })
 //Get: Getting one data
+router.get('/:id', async (req, res)=>{
+    const collection = await db.collection('sales')
+    let query = {_id:new ObjectId(req.params.id)}
+    const result = await collection.findOne(query)
+// for server not found (404)
 
+
+if(!result)
+res.send('Not Found').status(404)
+else res.send(result).status(200)
+})
 //Post: Posting one data
+router.post('/', async (req, res)=>{
+    const collection = await db.collection('sales')
+    const newDocument = req.body // it should have unique key value
+console.log(newDocument, req.body)
+newDocument.date = new Date() // gives date and time of creating new database
+const result = await collection.insertOne(newDocument)
 
+
+res.send(result).status(204)
+
+
+})
 //Patch: Updating one data
 
+
 //Delete: Deleting one data
+
 
 
 
